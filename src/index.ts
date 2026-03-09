8e9d654f3832789650fd383a3534868dba33c868
unknown <chrezna@gmail.com>
2026-03-09 12:53:00 +0900

    add file extensions

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { db } from "./db";
 import cors from "cors";
 import securityMiddleware from "./middleware/security.js";
 import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = 8000;
